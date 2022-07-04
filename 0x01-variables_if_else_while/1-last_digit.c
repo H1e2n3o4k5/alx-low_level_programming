451bc668f1de34fa2ae4532bee33ac965544385a
Henok Yimer <henokyimer79@gmail.comgit config --global user.email henokyimer79@gmail.comd>
2022-07-04 16:29:42 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,18 +15,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("Last digit of %d is ", n);
-	if (n > 5)
+	printf("Last digit of %d is %d", n);
+	if (n % 10 > 5)
 	{
-		printf("greater than 5");
+		printf("and is greater than 5");
 	}
-	if (n == 0)
+	if (n % 10 == 0)
 	{
 		printf("0");
 	}
-	if (n < 6 && n != 0)
+	if (n % 10 < 6 && n % 10 != 0)
 	{
-		printf("less than 6 not 0");
+		printf("and is less than 6 not 0");
 	}
 
 	printf("\n");
