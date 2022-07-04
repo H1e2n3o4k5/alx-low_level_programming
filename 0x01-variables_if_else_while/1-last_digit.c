067b95dfbcec64ce32b944d7a0409e7d7d6b5840
Henok Yimer <henokyimer79@gmail.comgit config --global user.email henokyimer79@gmail.comd>
2022-07-04 16:36:32 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 
 /**
- * main -> assign a random number to the variable n each time it is executed
+ * main - assign a random number to the variable n each time it is executed
  * and print the last digit of the number stored in the variable n
  *
  * Return : always 0
@@ -16,15 +16,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is %d", n);
-	if (n % 10 > 5)
+	if (n > 5)
 	{
 		printf("and is greater than 5");
 	}
-	if (n % 10 == 0)
+	if (n == 0)
 	{
 		printf("0");
 	}
-	if (n % 10 < 6 && n % 10 != 0)
+	if (n < 6 && n % 10 != 0)
 	{
 		printf("and is less than 6 not 0");
 	}
