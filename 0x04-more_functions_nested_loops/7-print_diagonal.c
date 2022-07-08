ad9795bd22e0338414bf082b08462e64503fdf77
Henok Yimer <henokyimer79@gmail.comgit config --global user.email henokyimer79@gmail.comd>
2022-07-08 14:21:33 -0700

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -4,25 +4,26 @@
  * @n: print int
  * Return: 0
  */
+
 void print_diagonal(int n)
 {
-	int i;
-	int b;
+int i;
+int b;
 
-	if (n > 0)
-	{
-		for (i = 0; i < n; i++)
-		{
-			for (b = 0; b < n; b++)
-			{
-				_putchar(' ');
-			}
-			_putchar(92);
-			_putchar('\n');
-		}
-	}
-	else
-	{
-		_putchar('\n');
-	}
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (b = 0; b < n; b++)
+{
+_putchar(' ');
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
