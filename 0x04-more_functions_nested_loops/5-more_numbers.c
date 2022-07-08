f1e3eabbefc88ec4456c7669471f53e5ca943890
Henok Yimer <henokyimer79@gmail.comgit config --global user.email henokyimer79@gmail.comd>
2022-07-08 13:30:44 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,16 +1,23 @@
 #include "main.h"
 /**
- * more_numbers - printing more numbers
+ * more_numbers - print numbers to 14
+ *
+ * Return: 0
  */
 void more_numbers(void)
 {
-	int x, y;
+int x, y;
 
-	for (x = 0; x < 10; x++)
-	{
-		for (y = 0; y < 15; y++)
-		{
-			_putchar(y + '0');
-		}
-		_putchar('\n')
-	}
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y < 15; y++)
+{
+if (y > 9)
+{
+_putchar((y / 10) + '0');
+}
+_putchar((y % 10) + '0');
+}
+_putchar(10);
+}
+}
