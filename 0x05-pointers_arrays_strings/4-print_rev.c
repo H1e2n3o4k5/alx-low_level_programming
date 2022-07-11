09343d2752e66ab38ec9fe53718fb2c07195531b
Henok Yimer <henokyimer79@gmail.comgit config --global user.email henokyimer79@gmail.comd>
2022-07-11 14:00:22 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * ptint_rev - function that prints a string, in reverse
+ * @s: this is input string
+ */
+void prini_rev(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; index++)
+		;
+	for (index = -1; s[index] != '\0'; index--)
+	{
+		_putchar(s[index]);
+	}
+	_putchar('\n');
+}
