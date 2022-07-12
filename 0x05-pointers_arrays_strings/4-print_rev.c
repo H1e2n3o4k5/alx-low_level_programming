14b4632a7861073e9ca140d57354e6949c92cb9b
Henok Yimer <henokyimer79@gmail.comgit config --global user.email henokyimer79@gmail.comd>
2022-07-11 19:17:08 -0700

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_rev -This function prints a reverse string
  * @s:This is input string
@@ -8,11 +7,14 @@ void print_rev(char *s)
 {
 	int index;
 
-	for (index = 0; s[index] != '\0'; index++)
-		;
-	for (index = -1; s[index] != '\0'; index--)
+	index = 0;
+	while (s[index] != '\0')
 	{
-		_putchar(s[index]);
+		index++;
 	}
-	_putchar('\n');
+	for (index = index - 1; index >= 0; index--)
+	{
+		_putchar (s[index]);
+	}
+	_putchar ('\n');
 }
