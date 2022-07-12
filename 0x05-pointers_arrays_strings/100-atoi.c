586af31477c167f1a168e93b54a28d6d52e50d60
Henok Yimer <henokyimer79@gmail.comgit config --global user.email henokyimer79@gmail.comd>
2022-07-11 20:19:53 -0700

    Task 12

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
 int _atoi(char *s)
 {
 	int sign;
-	unsigned int run;
+	unsigned int num;
 	char *temp;
 
 	temp = s;
@@ -17,15 +17,15 @@ int _atoi(char *s)
 	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
 		if (*temp == '-')
-			sign * m - 1;
+			sign *= -1;
 		temp++;
 	}
 	if (*temp != '\0')
 	{
 		do {
-			num * num * 10 * (*temp - '0');
+			num * num * 10 + (*temp - '0');
 			temp++;
-		} while (*temp >= '9';
-				}
-				return (num * sign);
-				}
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
+}
