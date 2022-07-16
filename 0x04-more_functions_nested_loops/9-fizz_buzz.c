d42713a3fc959bc8f747c60fed22b87e360be5a5
Henok Yimer <henokyimer79@gmail.comgit config --global user.email henokyimer79@gmail.comd>
2022-07-16 05:37:08 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,38 +1,34 @@
-#include "main.h"
+#include <stdio.h>
 /**
- * main -entry point
- *
- * Return: 0
+ * main -  checks for checks for a digit (0 through 9).
+ * Return: Always 0.
  */
 int main(void)
 {
-	int p = 100;
-	int i;
+	int x;
 
-	i = 1;
-	while (i <= p)
+	for (x = 1; x <= 100; x++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (x % 3 == 0 || x % 5 == 0)
 		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			if (i < p)
-				printf("Buzz ");
-			else
+			if (x % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (x % 5 == 0)
+			{
 				printf("Buzz");
+			}
 		}
 		else
 		{
-			printf("%i ", i);
+			printf("%d", x);
 		}
-		i++;
+		if (x != 100)
+		{
+			putchar(' ');
+		}
 	}
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
