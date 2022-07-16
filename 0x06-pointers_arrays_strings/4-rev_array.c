1d0fb033d392db69b3b56b547134ba42ea369194
Henok Yimer <henokyimer79@gmail.comgit config --global user.email henokyimer79@gmail.comd>
2022-07-15 19:01:25 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * reverse_array - function that revers contnt of array
+ * reverse_array - function that reverses the content of an array of integers
  * @a: array
  * @n: number of elements
  */
-void reverse_array(int *a, int n)
+ivoid reverse_array(int *a, int n)
 {
 	int tmp, index;
 
