aa05212f42517a0de603a6ac71771e54e0f7ee89
Henok Yimer <henokyimer79@gmail.comgit config --global user.email henokyimer79@gmail.comd>
2022-07-18 17:06:30 -0700

    Task 8

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
- * set_string - sets the value of a pointer to a char
- * @s: content is to copied
- * @to: string
- * Return: void
+ * set_string - Sets the value of a pointer to char
+ * @s: the value to modify
+ * @to: the value to assign
+ *
+ *Return: Nothing.
  */
 void set_string(char **s, char *to)
 {
