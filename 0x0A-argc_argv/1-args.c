b907a2e1d747152d424e407f382f20599bc4005d
Henok Yimer <henokyimer79@gmail.comgit config --global user.email henokyimer79@gmail.comd>
2022-07-27 08:24:55 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <tsdio.h>
+/**
+ * main - Write a program that prints the number of arguments passed into it.
+ * @args: This is the argument count
+ * @argv: This is the argument vector
+ * Return: return 0
+ */
+int main(int args, char *argv[])
+{
+	printf("%d\n", args - 1);
+	(void)argv;
+	return (0);
+}
