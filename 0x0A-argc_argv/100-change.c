854e9d09163e8de37a79d73461daf3109d88f753
Henok Yimer <henokyimer79@gmail.comgit config --global user.email henokyimer79@gmail.comd>
2022-07-27 11:36:22 -0700

    Task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,40 +2,36 @@
 #include <stdlib.h>
 /**
  * main - prints coins left
- * @args: parameter entry
+ * @argc: parameter entry
  * @argv: one number of arguments
  * Return: 0
  */
-int main(int args, char *argv[])
+int main(int argc, char *argv[])
 {
 	int c, coins = o;
 
-	if (args != 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	c = atoi(argv[1]);
-	if (c < 0)
+	else
 	{
-		printf("0\n");
-		return (0);
-	}
-	for (; c >= 0;)
-	{
-		if (c >= 25)
-			c -= 25;
-		else if (c >= 10)
-			c -= 10;
-		else if (c >= 5)
-			c -= 5;
-		else if (c >= 2)
-			c -= 2;
-		else if (c >= 1)
-			c -= 1;
-		else
-			break;
-		coin += 1;
+		c = atoi(argv[1]);
+		while (c > 0)
+		{
+			if (c >= 25)
+				c -= 25;
+			else if (c >= 10)
+				c -= 10;
+			else if (c >= 5)
+				c -= 5;
+			else if (c >= 2)
+				c -= 2;
+			else if (c >= 1)
+				c -= 1;
+			coins += 1;
+		}
 	}
 	printf("%d\n", coins);
 	return (0);
