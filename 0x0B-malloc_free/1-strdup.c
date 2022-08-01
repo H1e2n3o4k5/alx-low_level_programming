ed4e7d395188fea52d370b9e6a9da9ebacdc6a59
Henok Yimer <henokyimer79@gmail.comgit config --global user.email henokyimer79@gmail.comd>
2022-08-01 10:39:25 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - copies the string given as parameter
+ * @str: string to duplicate
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int i, len;
+
+	i = 0;
+	len = 0;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	dup = malloc(sizeof(char) * (len + 1));
+	/*check if malloc succesful*/
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+	{
+		dup[i] = str[i]
+	}
+	dup[i] = '\0';
+	return (dup);
+}
