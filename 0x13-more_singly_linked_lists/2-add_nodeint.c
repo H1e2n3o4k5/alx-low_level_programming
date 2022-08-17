c55a39ef31aad7786ceea63bd95847ed1e73d92d
Henok Yimer <henokyimer79@gmail.comgit config --global user.email henokyimer79@gmail.comd>
2022-08-16 18:53:03 -0700

    Task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,17 +1,20 @@
 #include "lists.h"
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: pointer to head of the list
- * Return: number of nodes
+ * add_nodeint - adds a new node at the beginning of a linked list
+ * @head: pointer to head of the list
+ * @n: data to insert in that new node
+ *
+ * Return: pointer to the new node, or NULL if it fails
  */
-size_t listint_len(const listint_t *h)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	size_t num = 0;
+	listint_t *new;
 
-	while (h)
-	{
-		num++;
-		h = h->next;
-	}
-	return (num);
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
